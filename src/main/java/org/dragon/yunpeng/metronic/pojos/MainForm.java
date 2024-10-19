7f7d3259cc314b14f9e099323cb49db6fe7e1e19
Yunpeng Li <yli4@umbc.edu>
2024-10-19 13:14:51 -0400

    Demostration of Adding, Modifying, and Saving Records in a Dynamic List By Modal Form

--- src/main/java/org/dragon/yunpeng/metronic/pojos/MainForm.java
+++ src/main/java/org/dragon/yunpeng/metronic/pojos/MainForm.java
@@ -1,26 +1,31 @@
 package org.dragon.yunpeng.metronic.pojos;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainForm {
+public class MainForm implements Serializable {
 	private String mainInput;
-    private List<SubForm> subFormList = new ArrayList<SubForm>();
+	private List<SubForm> subFormList = new ArrayList<SubForm>();
 
-    // Getters and setters
-    public String getMainInput() {
-        return mainInput;
-    }
+	public MainForm() {
 
-    public void setMainInput(String mainInput) {
-        this.mainInput = mainInput;
-    }
+	}
 
-    public List<SubForm> getSubFormList() {
-        return subFormList;
-    }
+	// Getters and setters
+	public String getMainInput() {
+		return mainInput;
+	}
 
-    public void setSubFormList(List<SubForm> subFormList) {
-        this.subFormList = subFormList;
-    }
+	public void setMainInput(String mainInput) {
+		this.mainInput = mainInput;
+	}
+
+	public List<SubForm> getSubFormList() {
+		return subFormList;
+	}
+
+	public void setSubFormList(List<SubForm> subFormList) {
+		this.subFormList = subFormList;
+	}
 }
--- src/main/java/org/dragon/yunpeng/metronic/pojos/SubForm.java
+++ src/main/java/org/dragon/yunpeng/metronic/pojos/SubForm.java
@@ -1,29 +1,35 @@
 package org.dragon.yunpeng.metronic.pojos;
 
-public class SubForm {
+import java.io.Serializable;
+
+public class SubForm implements Serializable {
 	private String field1;
-    private String field2;
+	private String field2;
 
-    // Constructor
-    public SubForm(String field1, String field2) {
-        this.field1 = field1;
-        this.field2 = field2;
-    }
+	public SubForm() {
 
-    // Getters and setters
-    public String getField1() {
-        return field1;
-    }
+	}
 
-    public void setField1(String field1) {
-        this.field1 = field1;
-    }
+	// Constructor
+	public SubForm(String field1, String field2) {
+		this.field1 = field1;
+		this.field2 = field2;
+	}
 
-    public String getField2() {
-        return field2;
-    }
+	// Getters and setters
+	public String getField1() {
+		return field1;
+	}
 
-    public void setField2(String field2) {
-        this.field2 = field2;
-    }
+	public void setField1(String field1) {
+		this.field1 = field1;
+	}
+
+	public String getField2() {
+		return field2;
+	}
+
+	public void setField2(String field2) {
+		this.field2 = field2;
+	}
 }
